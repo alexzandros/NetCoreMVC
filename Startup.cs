a798f399e721c702c48decb1a3789e957300b547
Carlos Roberto Rojas <carlos.rojas@colombogermana.edu.co>
2018-01-10 11:26:08 -0500

    Se agrega CORS

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Http;
 
 namespace RegistroEstudiantesMVC
 {
@@ -26,6 +28,7 @@ namespace RegistroEstudiantesMVC
             services.AddDbContext<EstudianteNS.DALContext>();
             services.AddDbContext<EstudianteNS.DALMySQLContext>();
             services.AddResponseCompression();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +45,8 @@ namespace RegistroEstudiantesMVC
 
             app.UseStaticFiles();
             app.UseResponseCompression();
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyHeader());
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
